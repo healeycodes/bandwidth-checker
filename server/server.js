35049a058aaa83659622c1777661b9959e1962d0
Andrew <healeycodes@gmail.com>
2019-08-18 09:11:09 +0100

    Add server files from Glitch

---
+++ server/server.js
@@ -0,0 +1,49 @@
+// local database
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+
+db.defaults({ results: [] }).write();
+
+// express server
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+// get bandwidth test results for graphing here
+app.get("/read", function(request, response) {
+  const data = db.get("results").value();
+  const prepared = data.map(s => {
+    return { x: s.date, y: s.speed };
+  });
+  const trimmed = prepared.slice(Math.max(prepared.length - 10, 1));
+  response.send(trimmed); // send last ten results
+});
+
+// send bandwidth test results here
+app.post("/save", function(request, response) {
+  // not secure against timing-based attacks!
+  if (request.body.pw !== process.env.SECRET) {
+    return response.status(400).send("Bad pw");
+  }
+  db.get("results")
+    .push({
+      speed: request.body.speed,
+      unit: request.body.units,
+      date: request.body.date * 1000
+    }) // correct to JS time
+    .write();
+  response.send("OK");
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
